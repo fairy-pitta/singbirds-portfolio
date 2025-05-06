e0561091980c8ceabd192ea5cab2ac81638d35a4
Pitta <shuna120700@gmail.com>
2025-05-06 16:21:43 +0800

    remove unnecessary public images

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <Head>
+        <meta name="apple-mobile-web-app-title" content="SingBirds" />
         <link rel="icon" type="image/png" href="/fairy-pitta.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#0ea5e9" />
