eb502ad2740e7e6c305c72b4ff2af7d5b480a421
Pitta <shuna120700@gmail.com>
2025-07-12 18:34:21 +0800

    add postbuild command

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    runtime: 'edge',
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
